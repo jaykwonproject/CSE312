f94113077cef38390ccb68e831bbb6143bd60689
jaykwonproject <jaykwonproject@gmail.com>
2021-11-10 00:55:01 -0500

    Initial layout with working docker compose file #2

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ EXPOSE 8000
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait /wait
 RUN chmod +x /wait
 
-CMD /wait && python main.py
+CMD /wait && python server.py
--- server.py
+++ server.py
@@ -1,44 +1,31 @@
-from flask import Flask, render_template, request, redirect, Response, abort, url_for
-from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-import re, hashlib, uuid
+import flask
+from pymongo import MongoClient
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = 'login'
-
-# class User(username, password):
-#     u = username
-#     p = password
-
-db = []
 @app.route('/')
-# @app.route('/home')
+
 def home():
-    return render_template('index.html')
+    client = MongoClient('mongo',27017)
+    db = client.chatDatabase
+    collection = db.chatCollection
+    results = collection.find()
+    return flask.render_template('index.html', data=results)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    print(request.form['username'])
-    print(request.form['password'])
-
-    return redirect(url_for('home'))
-
-    # if form.validate_on_submit():
-    #     # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') hash and salt in another way
-    #     user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     flash('Your account has been created! You are now able to log in', 'success')
-    #     return redirect(url_for('login'))
-    # return render_template('register.html', title='Register', form=form)
-
-
+    client = MongoClient('mongo',27017)
+    db = client.chatDatabase
+    collection = db.chatCollection
+    username = flask.request.form['username']
+    password = flask.request.form['password']
+    collection.insert_one({"username": username, "password": password})
+    client.close()
+    return flask.redirect(flask.url_for('home'),code=301)
 
 @app.route('/<error>')
 def error(error):
     return f"404 not found. Page '{error}' does not exist."
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug = True)#change to false for production
+    app.run(host='0.0.0.0', port='8000')#change to false for production
